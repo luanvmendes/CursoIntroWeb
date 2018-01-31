2a8c215bf07d710f235c03a461524aa62992eb69
luanvmendes <luanvmendes2@gmail.com>
2018-01-30 22:48:52 -0200

    Adicionando arquivos

---
+++ PortalSMA/js/menu-fixo.js
@@ -0,0 +1,13 @@
+/*Faz com que o menu seja fixado a partir do momento que ele encontre o topo da página*/
+window.onscroll = function() {myFunction()};
+
+var navbar = document.getElementById("navbar");
+var sticky = navbar.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
